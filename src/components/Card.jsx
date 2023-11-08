ab65c6727bcd91b23b3e610f3ace68d7b1d57fb0
Shawn Patoka <dev-services@shawnpatoka.com>
2023-11-08 16:53:31 -0600

    added minHeight to top cars in admin to match each other

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ children }) {
   return (
-    <div className="card mt-3">
+    <div className="card mt-3" >
       <div className="card-body">
         {children}
       </div>
--- src/pages/admin/AdminHome.jsx
+++ src/pages/admin/AdminHome.jsx
@@ -16,22 +16,30 @@ function AdminHome() {
       <PageTitle title="Dashboard" />
       <div className="container">
         <div className="row d-flex align-self-stretch">
+          {/* start left column */}
           <div className="col-md-7">
-            <Card>
-              <h2>Work Orders by Status</h2>
-              <div className="row">
-                <div className="col-4"><CircularProgress value={45} color="#ffa626" statusName="Open" /></div>
-                <div className="col-4"><CircularProgress value={35} color="#2e88bf" statusName="In Progress" /></div>
-                <div className="col-4"><CircularProgress value={20} color="#15ba95" statusName="Ready for Review" /></div>
+            <div className="card mt-3" style={{ minHeight: '235px' }}>
+              <div className="card-body">
+                <h2>Work Orders by Status</h2>
+                <div className="row">
+                  <div className="col-4"><CircularProgress value={45} color="#ffa626" statusName="Open" /></div>
+                  <div className="col-4"><CircularProgress value={35} color="#2e88bf" statusName="In Progress" /></div>
+                  <div className="col-4"><CircularProgress value={20} color="#15ba95" statusName="Ready for Review" /></div>
+                </div>
               </div>
-            </Card>
+            </div>
           </div>
+          {/* end left column */}
+          {/* start right column */}
           <div className="col-md-5">
-            <Card>
-              <h2>7-Day Users' Work Order Count</h2>
-              <DayOfWeekChart />
-            </Card>
+            <div className="card mt-3" style={{ minHeight: '235px' }}>
+              <div className="card-body">
+                <h2>7-Day Users' Work Order Count</h2>
+                <DayOfWeekChart />
+              </div>
+            </div>
           </div>
+          {/* end right */}
         </div>
         <div className="row">
           <div className="col-md-12">
