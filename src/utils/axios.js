a93e1beca1a88bdb2c905faaca8298434ded77d4
Shawn Patoka <76219517+shawnpatoka@users.noreply.github.com>
2024-01-02 15:25:22 -0600

    changed API_URL to heroku

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
-const API_URL = "http://127.0.0.1:8000/api/v1"
+// TODO - add env vars with debug mode
+// const API_URL = "http://127.0.0.1:8000/api/v1"
+const API_URL = "https://aav-work-orders.herokuapp.com/api/v1"
 
 export const axiosInstance = axios.create({
     baseURL: API_URL,
