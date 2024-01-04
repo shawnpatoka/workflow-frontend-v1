b99138c394ba04f8a4ff26b895d9966cced53a90
Shawn Patoka <76219517+shawnpatoka@users.noreply.github.com>
2024-01-04 15:42:24 -0600

    updated BASE_URL for api

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 // TODO - add env vars with debug mode
-const BASE_URL = "http://127.0.0.1:8000/api/v1"
-// const BASE_URL = "https://aav-work-orders.herokuapp.com/api/v1"
+// const BASE_URL = "http://127.0.0.1:8000/api/v1"
+const BASE_URL = "https://aav-work-orders.herokuapp.com/api/v1"
 
 export default axios.create({
     baseURL: BASE_URL
